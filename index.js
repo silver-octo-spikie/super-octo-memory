4d112aa94c6209914db05ad67e19dd24a4cb6f5f
silver-octo-spikie <84238870+silver-octo-spikie@users.noreply.github.com>
2021-05-15 23:00:34 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const { GiveawaysManager } = require("discord-giveaways");
+// Starts updating currents giveaways
+client.giveawaysManager = new GiveawaysManager(client, {
+    storage: "./handlers/giveaways.json",
+    updateCountdownEvery: 5000,
+    default: {
+        botsCanWin: false,
+        embedColor: "#FF0000",
+        reaction: "🎉"
+    }
+});
+
+client.giveawaysManager.on("giveawayReactionAdded", (giveaway, member, reaction) => {
+    console.log(`${member.user.tag} entered giveaway #${giveaway.messageID} (${reaction.emoji.name})`);
+});
+
+client.giveawaysManager.on("giveawayReactionRemoved", (giveaway, member, reaction) => {
+    console.log(`${member.user.tag} unreact to giveaway #${giveaway.messageID} (${reaction.emoji.name})`);
+});
+
+client.giveawaysManager.on("giveawayEnded", (giveaway, winners) => {
+    console.log(`Giveaway #${giveaway.messageID} ended! Winners: ${winners.map((member) => member.user.username).join(', ')}`);
+});
